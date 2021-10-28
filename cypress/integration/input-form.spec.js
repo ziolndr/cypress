b8d81293489a1f5eecb377d7346dc1452f189371
ziolndr <ziolndr@gmail.com>
2021-10-28 15:45:20 -0700

    component changes to fix test failures

---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,25 @@
+describe('Input form', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+  it('focuses input on load', () => {
+    cy.focused()
+      .should('have.class', 'new-todo')
+  })
+
+  it('accepts input', () => {
+    const typedText = "Buy Soda"
+
+    cy.get('.new-todo')
+      .type(typedText)
+      .should('have.value', typedText)
+  })
+
+  context('Form submission', () => {
+    it.only('Adds a new todo on submit', () => {
+      cy.get('.new-todo')
+        .type('Buy a cat')
+        .type('{enter}')
+    })
+  })
+})
