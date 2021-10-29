3fa82690cadaeda98f696318f161bb69864779b5
ziolndr <ziolndr@gmail.com>
2021-10-29 10:09:40 -0700

    adding files to feature branch

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -16,10 +16,44 @@ describe('Input form', () => {
   })
 
   context('Form submission', () => {
-    it.only('Adds a new todo on submit', () => {
+    beforeEach(() => {
+      cy.server()
+    })
+
+    const itemText = 'Buy a cat'
+    it('Adds a new todo on submit', () => {
+      cy.route('POST', '/api/todos', {
+        name: itemText,
+        id: 1,
+        isComplete: false
+      })
+
       cy.get('.new-todo')
-        .type('Buy a cat')
+        .type(itemText)
         .type('{enter}')
+        .should('have.value', '')
+
+      cy.get('.todo-list li')
+        .should('have.length', 1)
+        .and('contain', itemText)
+    })
+
+    it('Renders an error message on failed submission', () => {
+      cy.route({
+        url: '/api/todos',
+        method: 'POST',
+        status: 500,
+        response: {}
+      })
+
+      cy.get('.new-todo')
+        .type('test{enter}')
+
+        cy.get('.todo-list li')
+          .should('not.exist')
+
+        cy.get('.error')
+          .should('be.visible')
     })
   })
 })
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import Footer from './Footer'
-
+import {saveTodo} from '../lib/service'
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -14,10 +14,22 @@ export default class TodoApp extends Component {
       todos: []
     }
     this.handleNewTodoChange = this.handleNewTodoChange.bind(this)
+    this.handleTodoSubmit = this.handleTodoSubmit.bind(this)
   }
 
 handleNewTodoChange (evt) {
   this.setState({currentTodo: evt.target.value})
+}
+
+handleTodoSubmit (evt) {
+  evt.preventDefault()
+  const newTodo = {name: this.state.currentTodo, isComplete: false}
+  saveTodo(newTodo)
+    .then(({data}) => this.setState({
+      todos: this.state.todos.concat(data),
+      currentTodo: ''
+    }))
+    .catch(() => this.setState({error: true}))
 }
 
   render () {
@@ -26,8 +38,10 @@ handleNewTodoChange (evt) {
         <div>
           <header className="header">
             <h1>todos</h1>
+            {this.state.error ? <span className='error'>Whoops!</span> : null}
             <TodoForm
               currentTodo={this.state.currentTodo}
+              handleTodoSubmit={this.handleTodoSubmit}
               handleNewTodoChange={this.handleNewTodoChange}/>
           </header>
           <section className="main">
